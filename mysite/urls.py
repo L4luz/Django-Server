b17c63e21733383a9c18c2a2a0e7f339eaa62014
L4luz <80929906+L4luz@users.noreply.github.com>
2022-11-29 22:23:57 -0300

    adasd

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
- from . import views
+ from mysite.views import inicio
 
 urlpatterns = [
     path('', views.inicio),
--- mysite/view.py
+++ mysite/view.py
@@ -3,3 +3,6 @@ from django.shortcucuts import render,redirect
 def inicio(request):
     return render(request,"index.html")
     
+def modelo(request)
+    loaded_model =pickle.load(open("finalized_model.sav",'rb'))
+    y_pred=loaded_model.predict(x_test_bow)
