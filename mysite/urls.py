7a33885a1facc6fc6e38e2aec7122434130e070e
L4luz <80929906+L4luz@users.noreply.github.com>
2022-11-29 22:27:19 -0300

    asdasd

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
- from mysite.views import inicio
+from . import views
 
 urlpatterns = [
     path('', views.inicio),
